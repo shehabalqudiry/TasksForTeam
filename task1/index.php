6501dbefb6435f4099f8299689dd6a97d6ea8806
shehabalqudiry <omran.shehab40@gmail.com>
2020-08-27 08:26:14 +0200

    .

--- task1/index.php
+++ task1/index.php
@@ -7,15 +7,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title><?php echo "Team BackEnd"; ?></title>
 </head>
 <body>
-<?php
-    echo "<h2> PHP From Html </h2>",
-    "<h2> Welcome Eng.Amr Gamal </h2>",
-    $text1, $text2;
-    $text1 = $text1 . ' 5 <h1/>';
-    echo $text1, $text2;
-?>
+    <div id="app">
+    <?php
+        // pass multi "" in Php Code
+        echo "<h2 class=\"heading\"> PHP From Html </h2>",
+        // use ''
+        "<h2 class='heading'> PHP From Html </h2>",
+        // use Concatenation
+        "<h2 class=" . "heading" ."> PHP From Html </h2>",
+        // use Tag In love and honor, the engineer, Amr Gamal, the team leader
+        "<h2> Welcome Eng.Amr Gamal </h2>",
+        $text1, $text2;
+        $text1 = $text1 . ' 5 <h1/>';
+        echo $text1, $text2;
+    ?>
+    </div>
 </body>
 </html>
