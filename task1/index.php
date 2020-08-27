991f50af146b0fd9013e29d964a184e4188e79d6
shehabalqudiry <omran.shehab40@gmail.com>
2020-08-27 08:25:56 +0200

    Add Variables and CSS

--- task1/index.php
+++ task1/index.php
@@ -1,4 +1,21 @@
 <?php
-    echo 'Hello PHP';
-
+    $text1 = "<h1> I Love PHP";
+    $text2 = "<h1> My Team is The best </h1>";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "Team BackEnd"; ?></title>
+</head>
+<body>
+<?php
+    echo "<h2> PHP From Html </h2>",
+    "<h2> Welcome Eng.Amr Gamal </h2>",
+    $text1, $text2;
+    $text1 = $text1 . ' 5 <h1/>';
+    echo $text1, $text2;
+?>
+</body>
+</html>
